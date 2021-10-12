84869e0e527e1e8b6f29a63247389b14dc19eb21
Dzmitry Lyshko <lyshko.dmitry@gmail.com>
2021-10-12 21:54:36 +0300

    Fix creating object using classes

--- unit6.js
+++ unit6.js
@@ -4,10 +4,15 @@
 
 // Создать 2 объекта: animal и cat, объект animal добавить свойство move, объект cat должен наследовать свойство move
 
-// const animal = {};
-// const cat = {};
-// animal.move = true;
-// cat.__proto__ = animal;
+const animal1 = {};
+const cat1 = {};
+animal1.move = true;
+cat1.__proto__ = animal1;
+
+console.log(cat1.move);
+console.log(animal1.move);
+console.log(animal1);
+console.log(cat1);
 
 class Animal {
   constructor(move) {
@@ -15,8 +20,11 @@ class Animal {
   }
 }
 
-const cat = new Animal(true);
+class Cat extends Animal {}
+const cat = new Cat(true);
+const animal = new Animal(true);
 
 console.log(cat.move);
-console.log(Animal);
+console.log(animal.move);
+console.log(animal);
 console.log(cat);
